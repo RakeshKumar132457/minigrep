bb8d674b7169e43c20361841eefe89df6a99637e
Roydon Robbins <bruceisbatman4770@gmail.com>
2022-11-09 23:52:40 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use minigrep::Config;
+use std::{env, process};
+
+fn main() {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments : {err}");
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error : {e}");
+        process::exit(1);
+    }
+}
